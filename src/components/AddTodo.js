3a85065ae941b2c379d088c0b4a6418062262e99
yanisleidi <yaroga88@yahoo.com>
2018-04-07 19:24:49 -0700

    Use onSubmit instead of onClick for add a todo

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -2,16 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { addTodo } from '../actions';
 
+
 function AddTodo({ onAddTodo }) {
 	const inputRef = React.createRef();
+	
+	const handleSubmit = evt => {
+		const inputValue = inputRef.current.value;
+		
+		!!inputValue && onAddTodo(inputValue);
+		inputRef.current.value = '';
+		evt.preventDefault();
+	};
+		
 	return (
-		<div className="input-group mb-3">
-		<input ref={inputRef} type="text" className="form-control" placeholder="enter new todo"/>
-  			<div className="input-group-prepend">
-    			<button className="btn btn-danger" type="button" onClick={() => {
-					!!inputRef.current.value && onAddTodo(inputRef.current.value)
-				}}>Add to do</button>
-  			</div>
+		<div>
+			<form onSubmit={evt => handleSubmit(evt)} className="input-group mb-3">
+				<input ref={inputRef} type="text" className="form-control" placeholder="enter new todo"/>
+  				<div className="input-group-prepend">
+    				<button className="btn btn-danger" type="button" onClick={evt => handleSubmit(evt)}>Add to do</button>
+  				</div>
+			</form>
 		</div>
 	);
 }
